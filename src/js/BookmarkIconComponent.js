008b140549c6fc8244f50700871e3151f9bfcc5a
Menon, Dhanya <dhanya.menon@pearson.com>
2017-05-11 16:35:55 -0700

    Cleaning up localization

--- src/js/BookmarkIconComponent.js
+++ src/js/BookmarkIconComponent.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { IntlProvider } from 'react-intl';
-import InternationalSupport from './InternationalSupport';
+import { InternationalSupport } from '@pearson-incubator/aquila-js-core';
 import BookmarkIcon from './BookmarkIcon';
+import msgObject from '../../translations';
 
 export const BookmarkIconComponent = function BookmarkIconComponent(paramsObj) { // eslint-disable-line import/prefer-default-export
-  const intlObj = new InternationalSupport(paramsObj.locale);
+  const intlObj = new InternationalSupport(msgObject, paramsObj.locale);
   return (<IntlProvider locale={intlObj.getLocale()} messages={intlObj.getMessages()}>
     <BookmarkIcon
     	data={paramsObj.data}
